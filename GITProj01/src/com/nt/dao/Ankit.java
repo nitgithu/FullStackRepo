40e3259ff87a6ac8c65b69e947406ffe403dd9b3
DELL <DELL@DESKTOP-PHB6038>
2023-10-23 19:32:47 +0530

    Ankit.java commited

--- GITProj01/src/com/nt/dao/Ankit.java
+++ GITProj01/src/com/nt/dao/Ankit.java
@@ -4,8 +4,15 @@ public class Ankit {
 
 	public static void main(String[] args) {
 		System.out.println("Ankit.main()");
-		
-
+		int a=10;
+		int b=20;
+		int c=990;
+		int d=a+b+c;
+		System.out.println(d);
+		String s="*****Krishna*****";
+		String s1="krishna@123";
+		System.out.println(s);
+		System.out.println(s1);
 	}
 
 }
