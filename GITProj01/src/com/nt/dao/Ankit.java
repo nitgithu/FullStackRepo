1becccc2e4474d435de47e7142e231f0887864c6
DELL <DELL@DESKTOP-PHB6038>
2023-11-10 09:48:48 +0530

    updated

--- GITProj01/src/com/nt/dao/Ankit.java
+++ GITProj01/src/com/nt/dao/Ankit.java
@@ -4,5 +4,11 @@ public class Ankit {
   public void m1() {
 	  System.out.println("Ankit.m1()");
 	  System.out.println("created ankit class");
+	  System.out.println("krishna");
+  }
+  public void m2()
+  {
+	  System.out.println("Ankit.m2()");
+	  System.out.println("updated");
   }
 }
